632f2e4c271ec705aa824927ba90b542c53bc17c
anUnknownStoic <104714642+benbiji63@users.noreply.github.com>
2024-09-11 04:41:06 +0530

    Initial Commit

---
+++ intro.py
@@ -0,0 +1,13 @@
+# I/O
+
+print(':).')
+print(60*60*24*365)
+
+name = input("Enter your name \t")
+for i in range(0, 2):
+    print(name)
+
+name = input("Enter your name\t")
+exc = '!'
+print(exc+name+exc+name+exc)
+# print(f'{name}')
---
+++ mooc3.py
@@ -0,0 +1,26 @@
+# 3.1
+
+
+# Print numbers
+
+# i = 2
+# while i <= 30:
+#     print(i)
+#     i += 2
+
+# Fix the code: Countdown
+
+# print("Are you ready?")
+# number = int(input("Please type in a number: "))
+# while number != 0:
+#     print(number)
+#     number-=1
+# print("Now!")
+
+# Numbers
+
+# i = 1
+# number = int(input("Upper Limit: "))
+# while i < number:
+#     print(i)
+#     i += 1
